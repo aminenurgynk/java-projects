1f1e26970c2e0046e2d66ced178542e712a688da
Aminenur Göynük <amine.gynk@gmail.com>
2023-10-02 21:14:50 +0300

    Boost - Java Project
    Decision Control Structure

--- _02_DecisionControlStructure/src/main/java/com/aminenurgynk/Ex02.java
+++ _02_DecisionControlStructure/src/main/java/com/aminenurgynk/Ex02.java
@@ -1,4 +1,23 @@
 package com.aminenurgynk;
 
+/*
+Let's print `Hello` to the screen 5 times.
+ */
 public class Ex02 {
+
+    public static void main(String[] args) {
+
+// 1st way
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+// 2nd way
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+    }
+
 }
